597a421fc951ca3dfc4db4d4b97b053fc1e94429
Galzai <galzaidenstein@gmail.com>
2020-11-09 00:04:14 +0200

    Added initial project and started login implementation with firebase

---
+++ frontend/siinc-tv/src/components/UserSigning/signup.js
@@ -0,0 +1,18 @@
+/*
+ * This components will be used for user sign-up
+ */
+import React from 'react';
+
+export default class Signup extends React.Component
+{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return(
+            <h2>SIGN-UP TEST</h2>
+        );
+    }
+}
