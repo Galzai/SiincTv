409d06528d61be21c03ea862c2843d26be4e10be
BillSDev <mrtomboz@gmail.com>
2020-11-29 22:30:34 +0200

    Added rest of componenets and css for nav bar

--- frontend/siinc-tv/src/components/NavigationBar/userNavComponent.js
+++ frontend/siinc-tv/src/components/NavigationBar/userNavComponent.js
@@ -16,6 +16,12 @@ import {
        } 
        from "./navProfileMenuComponents.js"
 
+/*-----------------------------   Icons   -------------------------------*/
+import NoProfilePic from "../../assets/NoProfilePic.png"
+import NavNotificationIcon from "../../assets/NavBar/NavBarNotifications.png"
+import NavMessageIcon from "../../assets/NavBar/NavBarMessages.png"
+/*-----------------------------------------------------------------------*/
+
 function NavProfileMenu(props) {
     const userContext = useContext(UserContext);
 
@@ -45,7 +51,7 @@ function NavProfileMenu(props) {
 function NavProfileButton(props) {
     const userContext = useContext(UserContext);
     const imgSrc = ( userContext.user ) ? "https://img.icons8.com/material/4ac144/256/user-male.png"
-                                        : "https://cdn4.iconfinder.com/data/icons/linear-generic/200/linear-generic-user-myself-512.png";
+                                        : NoProfilePic;
     return(
         <div className={style.navProfileButton}>  
             <img 
@@ -58,43 +64,64 @@ function NavProfileButton(props) {
     );
 }
 
+function NavNotificationButton(props) {
+    return(
+        <div className={style.navNotificationButton}>
+                <img src={NavNotificationIcon}
+                     style={{width:"100%", height:"100%"}}
+                     onClick={() => props.onClick()}>
+                </img>
+        </div>
+    );
+}
+
+function NavMessageButton(props) {
+    return(
+        <div className={style.navMessageButton}>
+                <img src={NavMessageIcon}
+                     style={{width:"100%", height:"100%"}}
+                     onClick={() => props.onClick()}>
+                </img>
+        </div>
+    );
+}
+
 // represent user section of navigation bar ( sign in/sign up when logged out or notifications etc.. when logged in)
 function UserNavComponent(props) {
     const userContext = useContext(UserContext);
-    const[profileMenuOpen, setProfileMenuOpen] = useState(false);
-
-    //---------------- temporary ----------
-    const modalButtonStyle = {
-        marginTop:"20px", 
-        marginLeft:"15px", 
-        marginRight:"15px"
-    }
-    //-------------------------------------
+    const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+    const [messageOpen, setMessageOpen] = useState(false);
+    const [notificationOpen, setNotificationOpen] = useState(false);
 
     function doNothing(){
         console.log("nothing");
     }
 
     function handleProfileClick() {
-        if( profileMenuOpen ) {
-            setProfileMenuOpen(false);
-        }
-        else {
-            setProfileMenuOpen(true);
-        }
+        setProfileMenuOpen(!profileMenuOpen);
+    }
+
+    function handleMessageClick() {
+        setMessageOpen(!messageOpen);
+        console.log("mess = " + messageOpen);
+    }
+
+    function handleNotificationClick() {
+        setNotificationOpen(!notificationOpen);
+        console.log("notif = " + notificationOpen);
     }
 
     return(
         <div className={style.navUserComponent}>
-            {userContext.user && <NavProfileButton onClick={handleProfileClick}></NavProfileButton> }            
-            {/*!userContext.user && <NavProfileButton onClick={doNothing}></NavProfileButton> */}       
+            <NavProfileButton onClick={handleProfileClick}></NavProfileButton>             
             <SigningModal
                 user={userContext.user}
                 setUser={userContext.setUser}
-                refreshUserData={userContext.refreshUserData}
-                styles={modalButtonStyle}               
+                refreshUserData={userContext.refreshUserData}           
             >
             </SigningModal>
+            {userContext.user && <NavNotificationButton onClick={handleNotificationClick}></NavNotificationButton>}
+            {userContext.user && <NavMessageButton onClick={handleMessageClick}></NavMessageButton>}    
             {userContext.user && <NavProfileMenu open={profileMenuOpen}></NavProfileMenu> }       
         </div>
     );
