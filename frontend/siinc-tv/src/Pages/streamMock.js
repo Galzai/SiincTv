b17edf5f406ee41d2fd7fce929a2a731dd8d7115
Galzai <galzaidenstein@gmail.com>
2020-12-04 15:28:45 +0200

    Add stream details and user icons

--- frontend/siinc-tv/src/Pages/streamMock.js
+++ frontend/siinc-tv/src/Pages/streamMock.js
@@ -1,19 +1,23 @@
 import StreamViewBox from '../components/liveStream/streamingViewBox'
+import StreamDetails from '../components/liveStream/streamDetails'
 import {streamGroupsMock} from "../mocks/streamGroupsMock";
+import style from '../components/liveStream/liveStream.module.css'
 
 function StreamMock(props) {
     return(
-        <div>
+        <div className={style.StreamBox}>
             <StreamViewBox
                 currentStreamer="thehalfwayhouse"
                 streamGroups={streamGroupsMock}  
                 >
             </StreamViewBox>
+            <StreamDetails
+            streamTitle="Test stream title"
+            streamGroups={streamGroupsMock}  
+            >
+            </StreamDetails>
         </div>
-        
-        
-
-        
+           
     )
 }
 
---
+++ frontend/siinc-tv/src/components/liveStream/streamerCircle.js
@@ -0,0 +1,25 @@
+import style from './liveStream.module.css'
+import React, {useState} from "react";
+import ReactTooltip from 'react-tooltip';
+
+/**
+ * @brief This a circle describing the streamer
+ * @param {*} props 
+ */
+function StreamerCircle(props){
+
+    const streamer = props.streamer;
+    const imgSrc = ( streamer.image ) ? streamer.image : "https://img.icons8.com/material/4ac144/256/user-male.png"
+    ;
+    return(
+        <div data-tip={streamer.userName} className={style.streamerCircleDiv}>
+        <ReactTooltip />  
+        <img className={style.streamerCircle}
+            src={imgSrc} 
+        > 
+        </img>
+    </div>
+    );
+}
+
+export default StreamerCircle;
