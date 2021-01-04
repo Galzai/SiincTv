2887a10a4fe8fc8d8f13aa0d3593721869c87807
EdenAvnaim <eden@apiiro.com>
2021-01-05 01:31:35 +0200

    now working with split screen

--- frontend/siinc-tv/src/components/liveStream/splitScreenViewBox.js
+++ frontend/siinc-tv/src/components/liveStream/splitScreenViewBox.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import LiveStream from "./liveStream";
 import style from './liveStream.module.css'
+import Container from '@material-ui/core/Container'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
 function flatten(arr) {
     return arr.reduce(function (flat, toFlatten) {
@@ -11,22 +13,36 @@ function flatten(arr) {
 function SplitScreenViewBox(props){
 
     // The initial current streamer is the host
-    const [currentStreamer, setCurrentStreamer] = useState(props.currentStreamer);
     const [streamGroups, setStreamGroups] = useState(props.streamGroups);
 
+    const theme = createMuiTheme({
+        breakpoints: {
+          values: {
+            xs: 470,
+            sm: 1000,
+            md: 1240,
+            lg: 1440,
+            xl: 1920,
+          },
+        },
+      });
 
     function generateSplitStream(){
         const streamers = flatten(streamGroups);
         console.log(streamers);
         return(streamers.map(streamer=>{
             return(
-                <div className={style.splitScreen}>
-                <LiveStream  key={streamer.displayName}
-                streamer={streamer}
-                muted={true}
-             ></LiveStream>
-                </div>
-
+                <ThemeProvider theme={theme}>
+                        <Container maxWidth="xs"> 
+                            <div className={style.splitScreen}>
+                                <LiveStream
+                                    key={streamer.displayName}
+                                    streamer={streamer}
+                                    muted={true}
+                                ></LiveStream>
+                            </div>
+                        </Container>
+                </ThemeProvider>
             )}
         ));
     }
