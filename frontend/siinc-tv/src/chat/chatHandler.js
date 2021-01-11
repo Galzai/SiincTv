c4094dfc25f02380401fa8ac986c77b8388f3c41
Galzai <galzaidenstein@gmail.com>
2021-01-12 00:41:30 +0200

    Revert the revert

--- frontend/siinc-tv/src/chat/chatHandler.js
+++ frontend/siinc-tv/src/chat/chatHandler.js
@@ -13,8 +13,8 @@ const ChatHandler = (roomId) => {
       console.log(socketContext.streamRoomId);
       if(socketContext.streamRoomId == null)
       {
-        // The second socket will handler will handle this this
-        return;
+        socketContext.socket.emit(JOIN_ROOM, roomId);
+        socketContext.setStreamRoomId(roomId);
       }
 
       // Listens for incoming messages
@@ -31,6 +31,11 @@ const ChatHandler = (roomId) => {
         // leaves the room
         // when the connection is closed
       return () => {
+        if(socketContext.streamRoomId != null)
+        {
+          socketContext.socket.emit(LEAVE_ROOM,roomId);
+          socketContext.setStreamRoomId(null);
+        }
       };
   }
   }, []);
